34e4a8a0b716449a8077f30a0215306a548630ac
David <davidnunezparedes@gmail.com>
2022-10-26 17:46:11 -0400

    change

--- Prueba.c
+++ Prueba.c
@@ -4,15 +4,10 @@
 #use delay(clock=4M)
 #use i2c(Master,Fast=100000, sda=PIN_C7, scl=PIN_C6,force_sw)  
 
-
-
-
 #include "i2c_LCD.c"                                  // Libreria para el manejo del LCD
 #include "Teclado.c"                                  // Libreria para el manejo del teclado matricial 4x4
 #include "tonos_buzzer.c"                             // Libreria para el manejo de los Tonos del buzzer
 #include <math.h>
-
-
 
 #define USB_HID_DEVICE  TRUE
 #define USB_EP1_TX_ENABLE  USB_ENABLE_INTERRUPT     // Activar EP1 para transferencias masivas IN / interrupcion
@@ -35,17 +30,37 @@
 
 char K;
 int segundos=0,minutos=0;
-int contador=1, cont_submenu1=1,cont_submenu2=1;
-int menu=0,opcion=0,hinicio=0,hfinal=0,garaje=0;
+int contador=1;
+int menu=0,opcion=0;
 
-const char *MAIN_MENU_LABEL = "Main Menu";
-const char *WASH_MENU_LABEL = "Wash";
-const char *SHAKE_MENU_LABEL = "Shake";
-const char *PRIME_MENU_LABEL = "Prime";
-const char *PROGRAM_MENU_LABEL = "Program";
-const char *PROGRAM2_MENU_LABEL = "Program2";
-const char *WASH_WASH_MENU_LABEL = "Inner Wash";
-const char *WASH_PARAMS_MENU_LABEL = "Select Params";
+const char *MAIN_MENU_LABEL = "MENU PRINCIPAL";
+const char *WASH_MENU_LABEL = "LAVAR";
+const char *SHAKE_MENU_LABEL = "AGITAR";
+const char *PRIME_MENU_LABEL = "CEBAR";
+const char *PROGRAM_MENU_LABEL = "PROGRAMAR";
+
+const char *WASH_WASH_MENU_LABEL = "Lavar";
+const char *WASH_PARAMS_MENU_LABEL = "Selec Parametro";
+
+const char *WASH_WASHING_MENU_LABEL = "Lavando...";
+const char *WASH_PARAMS_FORMAT_MENU_LABEL = "F/Lavado:";
+const char *WASH_PARAMS_TIME_MENU_LABEL = "T/Enjuague:";
+const char *WASH_PARAMS_AMOUNT_MENU_LABEL = "C/Ciclos:";
+
+const char *SHAKE_SHAKE_MENU_LABEL = "Agitar";
+const char *SHAKE_MODE_MENU_LABEL = "Intensidad:";
+const char *SHAKE_TIME_MENU_LABEL = "T/Agitacion:";
+
+const char *PRIME_PRIME_MENU_LABEL = "Cebar";
+const char *PRIME_MODE_MENU_LABEL = "M/Cebado:";
+const char *PRIME_TIME_MENU_LABEL = "T/Cebado:";
+
+
+const char *PROGRAM_CREATE_MENU_LABEL = "Crear";
+const char *PRPGRAM_EDIT_MENU_LABEL = "Editar";
+const char *PROGRAM_DELET_MENU_LABEL = "Borrar";
+
+const char *PROGRAM_name_MENU_LABEL = "Nombre";
 
 typedef struct TMenu {
   char *label;
@@ -66,7 +81,6 @@ void initMenu(pMenu item, char *label) {
    item->label = label;
 }
 
-
 void addItem(pMenu parent, pMenu child) {
    if (parent->childrenCount >= 100) return;
    parent->children[parent->childrenCount] = child;
@@ -84,10 +98,15 @@ void testMenu() {
 }
 
 //declarations
-char tecla ();
+char read_key ();
 pMenu listenToKey(pMenu menu);
 void printMenu(pMenu menu);
 int min(int, int);
+
+
+//Operations
+void wash();
+
 
 ///**************************************************************************///
 ///******************** FUNCION MENU PRINCIPAL ******************************///
@@ -127,7 +146,7 @@ void printMenu(pMenu menu) {
 }
 
 pMenu listenToKey(pMenu menu) {
-   k = tecla();
+   k = read_key ();
    char ks[2];
    ks[0] = (char)k;
    ks[1] = '\0';
@@ -155,115 +174,9 @@ void scaffoldMenu(pMenu menu) {
    }
 }
 
-
-void menu_principal(void)
-{
-         LCD_PUTC("\f");
-         lcd_gotoxy(3,1);
-         printf (lcd_putc,"MENU PRINCIPAL");
-         lcd_gotoxy(2,2);
-         printf (lcd_putc,"1-LAVAR");               
-         lcd_gotoxy(2,3);                   
-         printf (lcd_putc,"2-CEBAR");               
-         lcd_gotoxy(2,4);
-         printf (lcd_putc,"3-AGITAR");
-}
-//**************************** MENU LAVAR **********************************///
-///************************************************************************///
-void menu_lavar (void)
-{
-         lcd_putc("\f");
-         lcd_gotoxy(4,1);
-         printf (lcd_putc,"MENU LAVAR");                
-         lcd_gotoxy(1,2);
-         printf (lcd_putc,"1-Selec Parametros");
-         lcd_gotoxy(1,3);
-         printf (lcd_putc,"2-Lavar");                
-}
-///**************************** MENU CEBAR **********************************///
-///************************************************************************///
-void menu_cebar (void)
-{
-         lcd_putc("\f");
-         lcd_gotoxy(1,1);
-         printf (lcd_putc,"MENU CEBAR");                  
-         lcd_gotoxy(1,2);
-         printf (lcd_putc,"1-M/cebado:");
-         lcd_gotoxy(1,3);
-         printf (lcd_putc,"2-T/cebado:");
-         lcd_gotoxy(1,4);
-         printf (lcd_putc,"3-Cebar:");
-}
-//**************************** MENU AGITAR **********************************///
-///**************************************************************************///
-void menu_agitar (void)
-{
-         lcd_putc("\f");
-         lcd_gotoxy(1,1);
-         printf (lcd_putc,"MENU AGITAR");
-         lcd_gotoxy(1,2);
-         printf (lcd_putc,"1-Intensidad:");
-         lcd_gotoxy(1,3);
-         printf (lcd_putc,"2-T/agitacion:");
-         lcd_gotoxy(1,4);
-         printf (lcd_putc,"3-Agitar");
-}
-//**************************** MENU PROGRAMAR *******************************///
-///**************************************************************************///
-void menu_programar (void)
-{
-            lcd_putc("\f");
-            lcd_gotoxy(1,1);
-            lcd_putc("MENU PROGRAMAR");
-            lcd_gotoxy(1,2);
-            lcd_putc("1-Crear");
-            lcd_gotoxy(1,3);
-            lcd_putc("2-Editar");
-            lcd_gotoxy(1,4);
-            lcd_putc("3-Borrar");
-}
-//******************************* SUBMENUS **********************************///
-///**************************************************************************///
-void submenu_lavar1 (void)
-{
-           lcd_putc("\f");
-           lcd_gotoxy(1,1);
-           lcd_putc("F/lavado:");
-           lcd_gotoxy(1,2);
-           lcd_putc("T/enjuague:");
-           lcd_gotoxy(1,3);
-           lcd_putc("Cant/ciclos:");
-}
-void submenu_lavar2()
-{
-           lcd_putc('\f');
-           lcd_gotoxy(1,1);
-           lcd_putc("Lavando...");
-           lcd_gotoxy(1,3);
-           lcd_putc("T/Lavado:");
-}
-void submenu_crear1 (void)
-{
-            lcd_putc("\f");
-            lcd_gotoxy(1,1);
-            lcd_putc("1-Nombre/Programa");
-            lcd_gotoxy(1,2);
-            lcd_putc("2-Formato/Lavado");
-            lcd_gotoxy(1,3);
-            lcd_putc("3-Volumen/Dispensar");
-            lcd_gotoxy(1,4);
-            lcd_putc("4-Tiemp/Enjuague");
-}
-void submenu_crear2(void)
-{
-            lcd_gotoxy(1,1);
-            lcd_putc("5-Cant/Ciclos");
-            lcd_gotoxy(1,2);
-            lcd_putc("6-Tipo/Aspiración:");
-}
 ///************************* FUNCION TECLA *********************************///
 ///************************************************************************///
-char tecla ()
+char read_key ()
    {
    Char c;
    do{
@@ -272,8 +185,6 @@ char tecla ()
    while(c=='\0');
    return(c);
 }
-///************************* FUNCION FLECHA ********************************///
-///*************************************************************************///
 
 void tiempo (void)
 {
@@ -324,84 +235,6 @@ void TECLADO_INTERRUPT(void)
    }
 } 
 
-void vselechora (void)
-{
-lcd_gotoxy(13,2);
-printf (lcd_putc,"%dH ",hinicio);
-lcd_gotoxy(13,3);
-printf (lcd_putc,"%dH ",hfinal);
-}
-
-void config_T_enjuague (void)
-{
-      if((cont_submenu1==1)&&(k=='#'))
-      {
-      hinicio++;
-      lcd_gotoxy(14,3);
-      printf (lcd_putc,"%dH ",hinicio);
-         if(hinicio==24)
-         {
-         hinicio=0;
-         if(hfinal==0)
-         {
-         hinicio=0;
-         if(hinicio==0)
-         {
-         lcd_gotoxy(14,3);
-         printf (lcd_putc,"%dH ",hinicio);
-         }
-         }
-         }
-      }
-         }
-
-void config_F_lavado (void)
-{
-            if((cont_submenu1==1)&&(k=='#'))
-            {
-            int garaje=0;
-            garaje=garaje+1;
-               if(garaje==1)
-              {
-              lcd_gotoxy(11,1);
-              lcd_putc("<UMELISA>");
-              }
-               if(garaje==2)
-              {
-              garaje=0;
-              lcd_gotoxy(11,1);
-              lcd_putc("<MicELISA>");
-              }
-            }
-}
-
-void config_F_lavado1(void)
-{
-         if (garaje==0)
-         {
-         lcd_gotoxy(11,1);
-         lcd_putc("<UMELISA>");
-         }
-         if (garaje==1)
-         {
-         lcd_gotoxy(11,1);
-         lcd_putc("<MicELISA>");
-         }
-        
-}
-void config_F_lavado2(void)
-{
-        if (garaje==1)
-         {
-         lcd_gotoxy(11,1);
-         lcd_putc("<UMELISA>");
-         }
-         if (garaje==0)
-         {
-         lcd_gotoxy(11,1);
-         lcd_putc("<MicELISA>");
-         }
-}
 void contador_s ()                                 //contador de 1s
 {
       do
@@ -445,24 +278,57 @@ void main()
    //delay_ms(500);                            // Es 1000ms para una espera de 4s
    lcd_putc("\f");
    
-   TMenu main, wash, prime, shake, program, program2, washWash, washParams;
+   TMenu main, wash, prime, shake, program, washWash, washParams, 
+   wash_format, wash_time, wash_amount, shake_shake, shake_mode,
+   shake_time, prime_prime, prime_mode, prime_time, 
+   program_create, program_edit, program_delet, program_name;
+   
+   
+   
    initMenu(&main, MAIN_MENU_LABEL);
    initMenu(&wash, WASH_MENU_LABEL);
    initMenu(&prime, PRIME_MENU_LABEL);
    initMenu(&shake, SHAKE_MENU_LABEL);
+   initMenu(&program, PROGRAM_MENU_LABEL);
+   
    initMenu(&washWash, WASH_WASH_MENU_LABEL);
    initMenu(&washParams, WASH_PARAMS_MENU_LABEL);
-   initMenu(&program, PROGRAM_MENU_LABEL);
-   initMenu(&program2, PROGRAM2_MENU_LABEL);
+   initMenu(&wash_format, WASH_PARAMS_FORMAT_MENU_LABEL);
+   initMenu(&wash_time, WASH_PARAMS_TIME_MENU_LABEL);
+   initMenu(&wash_amount, WASH_PARAMS_AMOUNT_MENU_LABEL);
+   initMenu(&shake_shake, SHAKE_SHAKE_MENU_LABEL);
+   initMenu(&shake_mode, SHAKE_MODE_MENU_LABEL);
+   initMenu(&shake_time, SHAKE_TIME_MENU_LABEL);
+   initMenu(&prime_prime, PRIME_PRIME_MENU_LABEL);
+   initMenu(&prime_mode, PRIME_MODE_MENU_LABEL);
+   initMenu(&prime_time, PRIME_TIME_MENU_LABEL);
+   initMenu(&program_create, PROGRAM_CREATE_MENU_LABEL);
+   initMenu(&program_edit, PRPGRAM_EDIT_MENU_LABEL);
+   initMenu(&program_delet, PROGRAM_DELET_MENU_LABEL);
+   initMenu(&program_name, PROGRAM_NAME_MENU_LABEL);
 
    
    addItem(&main, &wash);
    addItem(&main, &prime);
    addItem(&main, &shake);
    addItem(&main, &program);
-   addItem(&main, &program2);
+   
    addItem(&wash, &washWash);
    addItem(&wash, &washParams);
+   addItem(&washParams, &wash_format);
+   addItem(&washParams, &wash_time);
+   addItem(&washParams, &wash_amount);
+   addItem(&shake, &shake_shake);
+   addItem(&shake, &shake_mode);
+   addItem(&shake, &shake_time);
+   addItem(&prime, &prime_prime);
+   addItem(&prime, &prime_mode);
+   addItem(&prime, &prime_time);
+   addItem(&program, &program_create);
+   addItem(&program, &program_edit);
+   addItem(&program, &program_delet);
+   addItem(&program_create, &program_name);
+   
    
    scaffoldMenu(&main);
 }
